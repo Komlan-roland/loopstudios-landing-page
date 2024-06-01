707c9d2bd4be59c443fab62a06b6b3d5f3f133b2
Komlan Roland <102307875+Komlan-roland@users.noreply.github.com>
2024-06-01 12:39:36 +0000

    Add files via upload

--- loopstudios-landing-page-main/script.js
+++ loopstudios-landing-page-main/script.js
@@ -11,17 +11,24 @@ openMenu.addEventListener("click", () => {
 function hidemenu() {
     sideMenu.style.right = "-100%"
     closeMenu.style.display = "none";
-    openMenu.style.display = "block"
+    openMenu.style.display = "block";
 }
 
 closeMenu.addEventListener("click", hidemenu);
 
-sideMenu.querySelectorAll("a").forEach(link => {
-    link.addEventListener("click", hidemenu);
-});
-
 document.addEventListener("click", (event) => {
-    if (!sideMenu.contains(event.target) && event.target != openMenu) {
+    if (window.innerWidth < 875 && !sideMenu.contains(event.target) && event.target != openMenu) {
+        hidemenu();
+    }
+});
+window.addEventListener("scroll", (event) => {
+    if (window.innerWidth < 875) {
         hidemenu();
     }
 })
+
+window.addEventListener('resize', function () {
+    if (window.innerWidth > 875) {
+        openMenu.style.display = 'none';
+    } else { openMenu.style.display = 'block' }
+});
